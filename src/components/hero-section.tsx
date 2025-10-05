22b194aa8232df250a026203ad13670774680e65
ArkadiNazarian <nazarianarkadi@gmail.com>
2025-10-05 14:01:28 +0330

    fix the hero section image

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -7,14 +7,16 @@ export const HeroSection = () => {
     return (
         <div
             style={{
-                backgroundImage: "url('/svg/hero-section-background.svg')",
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-                backgroundRepeat: 'no-repeat',
+                // backgroundImage: "url('/svg/hero-section-background.svg')",
+                // backgroundSize: 'cover',
+                // backgroundPosition: 'center',
+                // backgroundRepeat: 'no-repeat',
+                background: 'linear-gradient(180deg, rgba(14, 182, 195, 0.00) 0%, rgba(112, 203, 216, 0.55) 100%), linear-gradient(75deg, rgba(6, 62, 67, 0.50) 10.07%, rgba(66, 154, 166, 0.15) 91.43%)',
                 width: '100%',
-                height: '100vh'
+                height: '100vh',
+                // borderRadius: '0 0 250px 250px',
             }}
-            className="md:px-2 px-12 xl:px-24 relative"
+            className="md:px-2 px-12 xl:px-24 relative rounded-[0_0_20%_20%] overflow-hidden"
         >
             <div className="pt-6 flex items-center justify-between ">
                 <div className="flex items-center gap-10">
@@ -23,7 +25,7 @@ export const HeroSection = () => {
                         alt="Alluna Dental Logo"
                         width={150}
                         height={50}
-                        
+
                     />
                     <Link href="#" className="text-base font-semibold text-dark text-nowrap">About Us</Link>
                     <Link href="#" className="text-base font-semibold text-dark">Veneers</Link>
@@ -50,21 +52,23 @@ export const HeroSection = () => {
                         <p className="text-space-blue font-bold text-lg">Award Winning Veneers </p>
                     </div>
 
-                    <p className="font-bold text-5xl 2xl:text-[64px] text-white pt-10">A Natural, <br />Confident Smile <br />Designed Just for You</p>
+                    <p className="font-bold text-4xl 2xl:text-[64px] text-white pt-10">A Natural, <br />Confident Smile <br />Designed Just for You</p>
                     <p className="font-bold text-xl 2xl:text-2xl text-white pt-6">From Studio City to Beverly Hills, see why hundreds of <br />patients choose Alluna Dental for natural results, luxury <br />comfort, and world-class smile makeovers.</p>
                     <div className="flex items-center justify-center gap-4 mt-14 w-max bg-space-blue rounded-2xl px-8 py-5">
                         <p className="text-lg font-bold text-white">Begin Your Smile Journey</p>
                         <ArrowRightIcon className="text-white" />
                     </div>
                 </div>
-                <div className="w-[48%] h-[93%] absolute bottom-0 right-0 2xl:right-12">
-                    <Image
-                        src="/svg/hero-section-smile.svg"
-                        alt="Hero Section Image"
-                        width={500}
-                        height={500}
-                        className="w-full h-full"
-                    />
+                <div className="absolute bottom-0 right-0 flex justify-end items-end w-full h-full pointer-events-none">
+                    <div className="relative w-[60%] lg:w-[65%] xl:w-[60%] 2xl:w-[55%] max-h-[94%] min-w-[300px] aspect-[1/1] md:aspect-[4/5]">
+                        <Image
+                            src="/svg/hero-section-smile.svg"
+                            alt="Hero Section Image"
+                            fill
+                            className="object-contain object-bottom"
+                            priority
+                        />
+                    </div>
                 </div>
                 <div className="bg-white p-3 2xl:p-5 rounded-3xl w-max flex flex-col items-center justify-center absolute right-12 xl:right-24 bottom-6 2xl:bottom-28">
                     <Image
