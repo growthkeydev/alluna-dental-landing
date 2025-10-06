b7398d7945c34013f74d62efce5cc77c04d6162d
ArkadiNazarian <nazarianarkadi@gmail.com>
2025-10-06 13:13:04 +0330

    add the small desktop responsive for experience section

--- src/components/experience-section.tsx
+++ src/components/experience-section.tsx
@@ -6,12 +6,27 @@ export const ExperienceSection = () => {
     return (
         <div
             style={{
-                backgroundImage: "url('/svg/experience-section-background.svg')",
-                backgroundSize: 'cover',
-                backgroundRepeat: 'no-repeat',
-
+                backgroundImage: `
+      /* Top fade */
+      linear-gradient(to bottom, #E9F4F5 0%, #E9F4F500 100%),
+      /* Bottom fade */
+      linear-gradient(to top, #E9F4F5 0%, #E9F4F500 100%),
+      /* Main overlay & image layers */
+      linear-gradient(to bottom, rgba(255, 255, 255, 0.8), transparent),
+      linear-gradient(to right, #266279, #9E8085),
+      url('/svg/experience-section-background.svg')
+    `,
+                backgroundSize: `
+      100% 200px,  /* top fade height */
+      100% 200px,  /* bottom fade height */
+      cover,
+      cover,
+      cover
+    `,
+                backgroundRepeat: 'no-repeat, no-repeat, no-repeat, no-repeat, no-repeat',
+                backgroundPosition: 'top, bottom, center, center, center',
             }}
-            className="px-32 pt-10 relative bg-gradient-to-r from-teal-blue to-dusty-rose flex flex-col items-center justify-center"
+            className="px-6 md:px-12 xl:px-24 relative  flex flex-col items-center justify-center mt-40"
         >
             <div>
                 <Image
@@ -22,32 +37,32 @@ export const ExperienceSection = () => {
                     className="w-full h-[600px]"
                 />
             </div>
-            <div className="flex items-center gap-11 mt-20">
-                <p className="text-white font-bold text-[46px] w-[520px]">Proven Experience in Smile Transformations</p>
-                <p className="text-white font-normal text-lg w-[588px]">With hundreds of smiles transformed in Studio City and beyond, Alluna Dental is trusted by patients who want natural results, luxury comfort, and precision care. From veneers to complete makeovers, our boutique approach has earned us 5-star reviews and national recognition.</p>
+            <div className="flex items-center justify-between pt-16">
+                <p className="text-white font-bold text-3xl xl:text-[46px] max-w-[520px] w-1/2">Proven Experience in Smile Transformations</p>
+                <p className="text-white font-normal text-base xl:text-lg max-w-[588px] w-1/2">With hundreds of smiles transformed in Studio City and beyond, Alluna Dental is trusted by patients who want natural results, luxury comfort, and precision care. From veneers to complete makeovers, our boutique approach has earned us 5-star reviews and national recognition.</p>
 
 
             </div>
             <div className="flex items-center justify-between w-full max-w-[1176px] mt-28">
 
                 <div>
-                    <p className="font-semibold text-[76px] text-white">780+</p>
-                    <p className="font-normal text-lg text-white">Smiles Transformed</p>
+                    <p className="font-semibold text-5xl xl:text-[76px] text-white">780+</p>
+                    <p className="font-normal text-base xl:text-lg text-white">Smiles Transformed</p>
                 </div>
 
                 <div>
-                    <p className="font-semibold text-[76px] text-white">560+</p>
-                    <p className="font-normal text-lg text-white">Appointments Booked Online</p>
+                    <p className="font-semibold text-5xl xl:text-[76px] text-white">560+</p>
+                    <p className="font-normal text-base xl:text-lg text-white">Appointments Booked Online</p>
                 </div>
 
                 <div>
-                    <p className="font-semibold text-[76px] text-white">340+</p>
-                    <p className="font-normal text-lg text-white">5-Star Reviews & Recognitions</p>
+                    <p className="font-semibold text-5xl xl:text-[76px] text-white">340+</p>
+                    <p className="font-normal text-base xl:text-lg text-white">5-Star Reviews & Recognitions</p>
                 </div>
             </div>
 
-            <div className="flex items-center justify-center gap-4 mt-16 w-max bg-white rounded-2xl px-8 py-5">
-                <p className="text-lg font-bold text-space-blue">Join 780+ Patients Who Already Transformed Their Smiles</p>
+            <div className="flex items-center justify-center gap-4 mt-16 w-max bg-white rounded-2xl px-8 py-5 mb-28">
+                <p className="text-base xl:text-lg font-bold text-space-blue">Join 780+ Patients Who Already Transformed Their Smiles</p>
                 <ArrowRightIcon className="text-space-blue" />
             </div>
         </div>
